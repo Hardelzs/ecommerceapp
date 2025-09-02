b0c9e09126452bd822b4e8232f97a858cb131998
Hardelz <135560555+Hardelzs@users.noreply.github.com>
2025-09-02 23:31:31 +0100

    Add nikelogo.png image to the project

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,11 +1,42 @@
 import 'package:flutter/material.dart';
 
-
 class IntroPage extends StatelessWidget {
   const IntroPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // logo
+            Image.asset('lib/images/nikelogo.png', height: 240,),
+            const SizedBox(height: 43),
+
+
+            // title
+            const Text(
+              'Just Do It',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 24),
+
+
+            // sub title
+            const Text(
+              'Find your favorite and premiun quality shoes in one place!',
+              style: TextStyle(fontSize: 16),
+            ),
+            const SizedBox(height: 30),
+
+
+            // start new button
+            ElevatedButton(onPressed: () {}, child: const Text('Get Started')),
+          ],
+        ),
+      ),
+    );
   }
 }
