c74e4b280c67dd4584fb47c3cc564ca8d8e552ae
Hardelz <135560555+Hardelzs@users.noreply.github.com>
2025-09-03 13:37:40 +0100

    Refactor intro page layout and update UI elements for improved user experience

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -6,35 +6,70 @@ class IntroPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey,
+      backgroundColor: Colors.grey[300],
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // logo
-            Image.asset('lib/images/nikelogo.png', height: 240,),
-            const SizedBox(height: 43),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          child: Column(
+            children: [
+              //logo
+              Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Image.asset('lib/images/nikelogo.png', height: 240,),
+              ),
+          
+              const SizedBox(height: 48,),
+          
+          
+              //title
+              Text(
+                'Just Do It',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  // color: Colors.grey[700],
+                ),
+              ),
 
+              const SizedBox(height: 24,),
+          
+          
+              //subtitle
+             Text(
+                'Brand new sneakers and custom kicks made just for you in premium quality.',
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.grey,
+                ),
+                textAlign: TextAlign.center,
+              ),
 
-            // title
-            const Text(
-              'Just Do It',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 24),
-
-
-            // sub title
-            const Text(
-              'Find your favorite and premiun quality shoes in one place!',
-              style: TextStyle(fontSize: 16),
-            ),
-            const SizedBox(height: 30),
-
-
-            // start new button
-            ElevatedButton(onPressed: () {}, child: const Text('Get Started')),
-          ],
+              const SizedBox(height: 48,),
+          
+          
+              //button
+              GestureDetector(
+                onTap: () =>Navigator.push(context, MaterialPageRoute(builder: (context) => const HomePage())),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[900],
+                    borderRadius: BorderRadius.circular(12)
+                  ),
+                  padding: const EdgeInsets.all(29),
+                  child: Center(
+                    child: const Text(
+                      'Shop Now',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
